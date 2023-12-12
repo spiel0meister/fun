fdb50171f14b353295b35f01822bfeee2b24ffd6
spiel_meister <soviczan7@gmail.com>
2023-12-12 17:10:16 +0100

    basic setup, added @get_file_path function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Error, ErrorKind};
+
+use std::env::args;
+use std::fs::read_to_string;
+
+fn get_file_path() -> std::io::Result<String> {
+    let args_vec: Vec<String> = args().collect();
+
+    if args_vec.len() < 2 {
+        return Err(Error::new(ErrorKind::InvalidInput, "Not enough arguments"));
+    }
+
+    Ok(args_vec[1].clone())
+}
+
+fn main() -> std::io::Result<()> {
+    let filepath = get_file_path()?;
+    let content = read_to_string(filepath)?;
+    println!("{}", content);
+    Ok(())
 }
